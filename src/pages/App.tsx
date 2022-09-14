a2381dbd78d9b209bfc8a239342df380f425bc74
Elias <e.wambugu192@gmail.com>
2022-09-13 18:25:47 -0700

    changed animation style

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,41 +1,45 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import { useContext } from "react";
 import { UserContext } from "../utils/UserContext";
 import Login from "./Login";
 import Order from "./Order";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const App = () => {
   const [user] = useContext<any>(UserContext);
   return (
     <>
-    <AnimatePresence>
-      <Flex w="100%" h="100vh" justify="center" align={"center"} p={50}>
-        <motion.div
-          animate={{
-            scale: user ? [1.5, 1] : [1.5, 1],
-          }}
-          style={{ width: "100%", height: "100%" }}
-          transition={{ duration: 2 }}
-        >
-          <Flex
-            justify="center"
-            w="100%"
-            height="100%"
-            rounded={10}
-            bgGradient="linear(to-br, #29DE69, #17803C)"
-            boxShadow={"2xl"}
+        <Flex w="100%" h="100vh" justify="center" align={"center"} >
+          <motion.div
+            animate={{
+              scale: [1.5, 1],
+            }}
+            style={{ width: "100%", height: "100%", padding: 50 }}
+            transition={{ duration: 2 }}
+            
           >
-            {user ? (
-              <>
-                <Order />
-              </>
-            ) : null}
-          </Flex>
-        </motion.div>
-        {!user ? <Login /> : null}
-      </Flex>
-      </AnimatePresence>
+            <Flex
+              justify="center"
+              w="100%"
+              height="100%"
+              rounded={10}
+              bgGradient="linear(to-br, #29DE69, #17803C)"
+              boxShadow={"2xl"}
+            >
+              {user ? (
+                <>
+                  <motion.div
+                    animate={{opacity: [0, 1]}}
+                    transition={{ delay: 2 }}
+                  >
+                    <Order />
+                  </motion.div>
+                </>
+              ) : null}
+            </Flex>
+          </motion.div>
+          {!user ? <Login /> : null}
+        </Flex>
     </>
   );
 };
