6e87b75e2785a4d220146f132b518bb086a344e6
Elias <e.wambugu192@gmail.com>
2022-10-14 10:31:34 -0700

    finished backbtn component

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -1,14 +1,22 @@
-import {BiArrowBack} from 'react-icons/bi'
-import { useDarkGray } from '../utils/theme'
+import { Flex, Icon, Text } from "@chakra-ui/react";
+import { BiArrowBack } from "react-icons/bi";
+import { useDarkGray } from "../utils/theme";
 
-const BackBtn = () => {
-  const darkGray = useDarkGray()
+const BackBtn = ({ text }: { text?: string }) => {
+  const darkGray = useDarkGray();
+  
   return (
     <>
-      <BiArrowBack size={30} color={darkGray} />
+      <Flex color={darkGray} _hover={{cursor: 'pointer', color: 'gray.800'}}>
+        <Icon h={30} w={30} as={BiArrowBack} />
+        {text ? (
+          <Text ml={3} alignSelf={"center"} fontSize={20}>
+            {text}
+          </Text>
+        ) : null}
+      </Flex>
     </>
-  )
+  );
+};
 
-}
-
-export default BackBtn
+export default BackBtn;
--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,14 +1,16 @@
 import { Text, Button, Flex } from "@chakra-ui/react";
 import BackBtn from "../components/BackBtn";
 import PageContainer from "../components/PageContainer";
+import { useDarkGray } from "../utils/theme";
 
 const Checkout = () => {
+  const darkGray = useDarkGray();
   return (
     <>
       <PageContainer>
-        <Flex>
-          <BackBtn />
-          <Text>Checkout</Text>
+        <Flex color={darkGray}>
+          <BackBtn text="Continue Shopping" />
+          
         </Flex>
         <Button>hi</Button>
       </PageContainer>
