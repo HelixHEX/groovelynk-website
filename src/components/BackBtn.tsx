7e70036fcb9281dae8263a86e20ff915cdd7026a
Elias <e.wambugu192@gmail.com>
2022-10-14 10:34:07 -0700

    removed unused import

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -1,13 +1,18 @@
 import { Flex, Icon, Text } from "@chakra-ui/react";
 import { BiArrowBack } from "react-icons/bi";
+import {  useNavigate } from "react-router-dom";
 import { useDarkGray } from "../utils/theme";
 
 const BackBtn = ({ text }: { text?: string }) => {
   const darkGray = useDarkGray();
-  
+  const navigate = useNavigate();
   return (
     <>
-      <Flex color={darkGray} _hover={{cursor: 'pointer', color: 'gray.800'}}>
+      <Flex
+        onClick={() => navigate(-1)}
+        color={darkGray}
+        _hover={{ cursor: "pointer", color: "gray.800" }}
+      >
         <Icon h={30} w={30} as={BiArrowBack} />
         {text ? (
           <Text ml={3} alignSelf={"center"} fontSize={20}>
