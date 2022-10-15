b53e7b229e5f89b3b0a89c0901334e6f5b4d2987
Elias <e.wambugu192@gmail.com>
2022-10-15 18:45:58 -0400

    added screenshot functionality | moved gradient selector to shopping cart header

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,35 @@
-import { Heading } from "@chakra-ui/react";
+import { Button, Heading, useColorModeValue } from "@chakra-ui/react";
+import { useState } from "react";
 import PageContainer from "../components/PageContainer";
 
 const App = () => {
+  const [duration, setDuration] = useState("month");
+  const btnBg = useColorModeValue("white", "#6096FD");
   return (
     <>
       <PageContainer>
-        <Heading color={'#6096FD'}>Spotlist.</Heading>
+        <Heading color={"#6096FD"}>Spotlist.</Heading>
+        <Button
+          bg={duration === "month" ? btnBg : "none"}
+          onClick={() => setDuration("month")}
+          variant={duration === "month" ? "solid" : "ghost"}
+        >
+          Last Month
+        </Button>
+        <Button
+          bg={duration === "sixmonths" ? btnBg : "none"}
+          onClick={() => setDuration("sixmonths")}
+          variant={duration === "sixmonths" ? "solid" : "ghost"}
+        >
+          Last 6 Months
+        </Button>
+        <Button
+          bg={duration === "alltime" ? btnBg : "none"}
+          onClick={() => setDuration("alltime")}
+          variant={duration === "alltime" ? "solid" : "ghost"}
+        >
+          All Time
+        </Button>
       </PageContainer>
     </>
   );
