7de4c5c46b3d4b8551ef53913717d02c3303c650
Elias <e.wambugu192@gmail.com>
2022-10-16 18:12:05 -0400

    removed btn from home page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,32 +5,10 @@ import PageContainer from "../components/PageContainer";
 const App = () => {
   const [duration, setDuration] = useState("month");
   const btnBg = useColorModeValue("white", "#6096FD");
-  
   return (
     <>
       <PageContainer>
         <Heading color={"#6096FD"}>Spotlist.</Heading>
-        <Button
-          bg={duration === "month" ? btnBg : "none"}
-          onClick={() => setDuration("month")}
-          variant={duration === "month" ? "solid" : "ghost"}
-        >
-          Last Month
-        </Button>
-        <Button
-          bg={duration === "sixmonths" ? btnBg : "none"}
-          onClick={() => setDuration("sixmonths")}
-          variant={duration === "sixmonths" ? "solid" : "ghost"}
-        >
-          Last 6 Months
-        </Button>
-        <Button
-          bg={duration === "alltime" ? btnBg : "none"}
-          onClick={() => setDuration("alltime")}
-          variant={duration === "alltime" ? "solid" : "ghost"}
-        >
-          All Time
-        </Button>
       </PageContainer>
     </>
   );
