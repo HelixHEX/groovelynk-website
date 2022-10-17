2a029da37fd840038200ee0bf0e6857f187d5e48
Christian Thompson <ChristianThompson1@hotmail.com>
2022-10-16 22:26:22 -0700

    changed loading animation for when loading the main page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,17 +1,13 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Heading } from "@chakra-ui/react";
+import React from "react";
+import { ScaleLoader } from "react-spinners";
 
 const Loading = () => {
   return (
     <>
-      <Flex
-        bg='#6096FD'
-        width="100%"
-        h="100vh"
-        justify={"center"}
-      >
-        <Text color='white' fontSize={40} alignSelf={"center"}>
-          Loading...
-        </Text>
+      <Flex bg="#6096FD" width="100%" h="100vh" justify={'center'} align="center">
+          <Heading mr={5} pb={0.5} color={'white'}>Loading</Heading>
+          <ScaleLoader color="white" />
       </Flex>
     </>
   );
